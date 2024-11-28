e2379af121a0811bc583912f97bbcdc74efdeca9
Matteo Bettiati <matteo.bet.38@gmail.com>
2024-11-28 15:55:09 +0100

    Chapter 1: implemented lightshift mechanic

--- LightShifter Source Code/Assets/Scripts/LightShift/LightShift.cs
+++ LightShifter Source Code/Assets/Scripts/LightShift/LightShift.cs
@@ -5,8 +5,12 @@ namespace LightShift
     public class LightShift : MonoBehaviour
     {
         [SerializeField] private UnityEngine.Camera mainCamera;
-        [SerializeField] private Color newBackgroundColor; 
-
+        [SerializeField] private Color newBackgroundColor;
+        
+        [SerializeField] private GameObject gridLight;
+        [SerializeField] private GameObject gridDark;
+        [SerializeField] private GameObject environment;
+        
         private Color _originalBackgroundColor;           
         private bool _isChanged;                           
 
@@ -15,8 +19,8 @@ namespace LightShift
             // Cache the original background color and hide the platform initially
             if (mainCamera != null)
                 _originalBackgroundColor = mainCamera.backgroundColor;
+            ShowLight();
         }
-
         void Update()
         {
             // Check for 'E' key press once per frame
@@ -33,9 +37,46 @@ namespace LightShift
                 // Toggle between original and new background colors
                 mainCamera.backgroundColor = _isChanged ? _originalBackgroundColor : newBackgroundColor;
             }
+            if (_isChanged)
+            {
+                ShowLight();
+            }
+            else
+            {
+                ShowDark();
+            }
             // Flip the toggle state
             _isChanged = !_isChanged;
         }
+        private void ShowLight()
+        {
+            foreach (Transform child in environment.transform)
+            {
+                if (child.gameObject == gridLight)
+                {
+                    child.gameObject.SetActive(true);
+                }
+                else
+                {
+                    child.gameObject.SetActive(false);
+                }
+            }
+        }
+        private void ShowDark()
+        {
+            foreach (Transform child in environment.transform)
+            {
+                if (child.gameObject == gridDark)
+                {
+                    child.gameObject.SetActive(true);
+                }
+                else
+                {
+                    child.gameObject.SetActive(false);
+                }
+            }
+        }
+
     }
 
 }
