5350af75fdf59a3d463f3cd8fe14ff504cf7e423
TrueMaicol <michi.cavicchio@hotmail.com>
2024-12-24 21:00:27 +0100

    Fixed issues arised from previous commit on light shift bug

--- LightShifter Source Code/Assets/Scripts/LightShift/LightShift_BugTest.cs
+++ LightShifter Source Code/Assets/Scripts/LightShift/LightShift_BugTest.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Tilemaps;
@@ -7,27 +8,28 @@ namespace LightShift
     public class LightShift_BugTest : MonoBehaviour
     {
         [SerializeField] private GameObject gridLight, gridDark;
-        [SerializeField] GameObject lightBackground, darkBackground;
-        private SortingGroup _lightSortingGroup, _darkSortingGroup;
+        private SortingGroup _darkSortingGroup;
         private CompositeCollider2D _lightTilemapsCollider, _darkTilemapsCollider;
+        private Collider2D[] _lightColliders, _darkColliders;
         private bool _isChanged;
         public bool _canChange;
         [SerializeField] private KeyCode lightShiftKey = KeyCode.LeftShift;
-
+        [SerializeField] private float shiftCooldown = 0.5f;
 
         private void Awake()
         {
-            _lightSortingGroup = gridLight.GetComponent<SortingGroup>();
-            _lightTilemapsCollider = gridLight.GetComponentInChildren<CompositeCollider2D>();
-
+            // _lightTilemapsCollider = gridLight.GetComponentInChildren<ColliderGroup>().gameObject.GetComponent<CompositeCollider2D>();
+            _lightColliders = gridLight.GetComponentsInChildren<Collider2D>();
             _darkSortingGroup = gridDark.GetComponent<SortingGroup>();
-            _darkTilemapsCollider = gridDark.GetComponentInChildren<CompositeCollider2D>();
+            // _darkTilemapsCollider = gridDark.GetComponentInChildren<ColliderGroup>().gameObject.GetComponent<CompositeCollider2D>();
+            _darkColliders = gridDark.GetComponentsInChildren<Collider2D>();
+
         }
 
 
         private void Start()
         {
-            ShowDark();
+            ShowLight();
             _canChange = true;
         }
         public void CanChange(bool canChange)
@@ -44,11 +46,19 @@ namespace LightShift
             this._canChange = true;
         }
 
+        private IEnumerator ShiftCooldown()
+        {
+            _canChange = false;
+            yield return new WaitForSeconds(shiftCooldown);
+            _canChange = true;
+        }
+
         private void Update()
         {
             if (Input.GetKeyDown(lightShiftKey) && _canChange)
             {
                 ToggleEnvironment();
+                StartCoroutine(ShiftCooldown());
             }
         }
 
@@ -64,29 +74,35 @@ namespace LightShift
 
         private void ShowLight()
         {
-            _lightTilemapsCollider.isTrigger = false;
-            _darkTilemapsCollider.isTrigger = true;
+            // _lightTilemapsCollider.isTrigger = false;
+            // _darkTilemapsCollider.isTrigger = true;
+            SetColliderTrigger(_lightColliders, false);
+            SetColliderTrigger(_darkColliders, true);
 
-            _darkTilemapsCollider.enabled = false;
-            // this is to ensure the collider is able to detect any collision 
-            _darkTilemapsCollider.enabled = true;
             _darkSortingGroup.sortingOrder = 0;
         }
 
         private void ShowDark()
         {
-            // lightBackground.SetActive(false);
-            // darkBackground.SetActive(true);
-            _darkTilemapsCollider.isTrigger = false;
-            _lightTilemapsCollider.isTrigger = true;
+            // _darkTilemapsCollider.isTrigger = false;
+            // _lightTilemapsCollider.isTrigger = true;
+            SetColliderTrigger(_darkColliders, false);
+            SetColliderTrigger(_lightColliders, true);
 
-            // this is to ensure the collider is able to detect any collision
-            _lightTilemapsCollider.enabled = false;
-            _lightTilemapsCollider.enabled = true;
             // 2 is to ensure that the dark grid is always on top of the light grid
             // THE LIGHT GRID HAS A DEFAULT SORTING LAYER OF 1
             _darkSortingGroup.sortingOrder = 2;
         }
+
+        private void SetColliderTrigger(Collider2D[] colliders, bool isTrigger)
+        {
+            foreach (Collider2D collider in colliders)
+            {
+                collider.isTrigger = isTrigger;
+            }
+        }
+
+
     }
 
 }
